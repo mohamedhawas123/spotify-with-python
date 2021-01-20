7074e970944e6b6cac91e10999f8f2311cde319f
saif <mohamedhawas123@gmail.com>
2021-01-20 17:23:34 +0200

    get url from spotify

--- base.py
+++ base.py
@@ -1,27 +1,49 @@
 import json
 import requests
 import secret
-
+from pprint import pprint
 
 class lastFmSpotify:
 
     def __init__(self):
-        self.token = secret.spotify_token
-        self.api_key = secret.last_fm_api
-        self.user_id = secret.spotify_user_id
+        self.token = secret.spotify_token()
+        self.api_key = secret.last_fm_api()
+        self.user_id = secret.spotify_user_id()
         self.spoitfy_headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self.token}"} 
         self.playlist_id = ""
         
+        
 
     def fetchSongsFromLastfm(self):
-        pass
-    
-    def get_uri_from_spoitfy(self):
+        params = {'limit': 20, 'api_key': self.api_key}
+        url = f"http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json"
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            print("something went wrong ")
+        res = response.json()
+        for item in res['tracks']['track']:
+            song = item['name'].title()
+            artist = item['artist']['name'].title()
+            self.get_uri_from_spoitfy(song, artist )
 
-        pass
+
+    
+    def get_uri_from_spoitfy(self, song, artist):
+        
+        url = f"https://api.spotify.com/v1/search?query=track%3A{song}+artist%3A{artist}&type=track&offset=0&limit=20"
+        response = requests.get(url, headers=self.spoitfy_headers)
+        res = response.json()
+        for item in res['tracks']['items']:
+            print(item)
+
+        
 
     def create_playlist(self):
-        pass
+        data ={
+            "name": "LastFm playlist",
+            "description": "بلاي لست جامده",
+            "public": False
+        }
 
    
 
@@ -30,3 +52,7 @@ class lastFmSpotify:
 
     def list_songs_in_playlist(self):
         pass
+
+
+d = lastFmSpotify()
+d.fetchSongsFromLastfm()
--- secret.py
+++ secret.py
@@ -1,9 +1,11 @@
 def spotify_user_id():
-    return ""
+    return "seifbond77"
+
 
 
 def spotify_token():
-    return ""
+    return "BQALisCpFDHbP8Nczt6WeVicn8EYSx3yQnb2kPWW-r6N2LzU9YB4ttklPBD47Hrb5EVYBPvnhpfLqCsnQKs1dqrCOJQ9hYFhQOqaI9V2FzzOxNrYPlep3F7ZGBMyWU07qRxg4izD3mnjLVUGdVWmv7qg-dfU1W04LQF0XIWmzTYANrTmi6p_LcCwyn-H_uLnquIN"
+
 
 def last_fm_api():
     return "72532253895c992b658aec169170a93a"
